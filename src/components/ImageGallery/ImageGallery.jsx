e5052798e114709dad0c7a5c1f9e90b4d053eae4
RomanNosulskyi <nosulskyiroman@gmail.com>
2023-06-05 12:28:46 +0300

    1st

---
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -0,0 +1,5 @@
+import { ImageGalleryList } from './ImageGallery.styled';
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+export const ImageGallery = ({}) => {
+  return;
+};
