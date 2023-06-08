888aeb57b8576ff3671b42f0e5cbfef792012fc9
RomanNosulskyi <nosulskyiroman@gmail.com>
2023-06-08 13:17:17 +0300

    2

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,23 @@
+import PropTypes from 'prop-types';
 import { ImageGalleryList } from './ImageGallery.styled';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-export const ImageGallery = ({}) => {
-  return;
+export const ImageGallery = ({ data, showModal }) => {
+  return (
+    <ImageGalleryList>
+      {data.map(({ id, webformatURL, largeImageURL }) => {
+        return (
+          <ImageGalleryItem
+            key={id}
+            webformatURL={webformatURL}
+            largeImageURL={largeImageURL}
+            showModal={showModal}
+          />
+        );
+      })}
+    </ImageGalleryList>
+  );
+};
+ImageGallery.propTypes = {
+  data: PropTypes.array,
+  showModal: PropTypes.func,
 };
